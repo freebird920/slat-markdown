394a2292bd0aa1b3ec084264675ba4915ee0ff0c
freebird920 <freebird_han@naver.com>
2024-04-25 22:33:22 +0900

    학교 도서관 인터넷 차단 크롬 익스텐션의 도메인 목록 수정

--- attachment/chrome-extention-school-library-internet-blocker/index.js
+++ attachment/chrome-extention-school-library-internet-blocker/index.js
@@ -5,7 +5,7 @@
  * @description 여기에 허용할 도메인을 추가하세요. 끝에는 ,를 붙여야합니다.
  * @example "https://example.com",
 */
-  const whiteList = [
+const whiteList = [
   // 여기에 도메인을 아래와 같이 입력하세요. (단 //는 제외하고 입력하세요.)
   // "https://example.com", 
   "https://read365.edunet.net",
@@ -15,6 +15,9 @@
   "https://www.slat.or.kr",
 ];
 
+/**
+ * @description 이 코드는 학교 도서관에서 사용하는 인터넷 차단기입니다.
+ */
 if (!whiteList.some((baseUrl) => window.location.href.startsWith(baseUrl))) {
   const html = document.querySelectorAll("html");
   html.forEach((a) => {
